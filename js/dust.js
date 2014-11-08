b5b63b79626bc5cf7b33430d8878dadba100ccc3
Sara Gorecki <sgorecki@gmail.com>
2014-11-08 02:56:33 -0500

    Add more stuff....So tired.

--- js/dust.js
+++ js/dust.js
@@ -4,25 +4,33 @@ var Star = function(){
 };
 
 Star.prototype.drawMass = function() {
-  $('.star').width(this.diameter);
-  $('.star').height(this.diameter);
+  $(".star").width(this.diameter);
+  $(".star").height(this.diameter);
 }
 
 $(document).ready(function(){
   var star = new Star();
 
-  $(document).on('keydown', function(e) {
+  $(document).on("keydown", function(e) {
     if (e.which === 32 && star.gameOver === false) {
+      $(".header").removeClass("topMargin");
       star.drawMass();
       star.diameter += 2;
-      star.drawMass();
     }
   });
 
-  $(document).on('keyup', function(e) {
+  $(document).on("keyup", function(e) {
     if (e.which === 32 && star.gameOver === false) {
       star.gameOver = true;
       checkStarType(star.diameter);
+      $(".info").removeClass("hidden");
     }
+  });
+
+  $("#playAgain").on("click", function() {
+    star.diameter = 0;
+    star.drawMass();
+    star.gameOver = false;
+    $(".info").addClass("hidden");
   });
 });
