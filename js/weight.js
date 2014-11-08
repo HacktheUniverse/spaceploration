a724cb48c42f51feb635eaadf38cbf8d96c210e0
Sara Gorecki <sgorecki@gmail.com>
2014-11-08 16:10:01 -0500

    Add textures to planets

--- js/weight.js
+++ js/weight.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
   var planetNum = 0;
+  $('.planet').css("background-image", "url(./images/texture_4.png)");
 
   $('input').on('keyup', function() {
     var earthWeight = $(this).val();
@@ -10,13 +11,12 @@ $(document).ready(function(){
   });
 
   $('.newPlanet').on('click', function() {
-    console.log('length: ' + gravityTypes.length);
-    console.log('planetNum' + planetNum);
     if (planetNum < gravityTypes.length-1) {
       planetNum++;
     } else {
       planetNum = 0;
     }
+    $('.planet').css("background-image", "url(./images/" + gravityTypes[planetNum].texture + ")");
     $('.thisPlanet').html(gravityTypes[planetNum].name);
     checkGravity(planetNum, $('input').val());
   })
