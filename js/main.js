40d257e01f555e21d41727c9a293d0208318f83c
Camille Houferak <camillehouferak@Camilles-MacBook-Pro.local>
2014-11-08 13:44:09 -0500

    finished planet bios, edited star bios, added randomize function, and added hover pointer to home image

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
-$(document).mousemove(function(e){
-    $("#space-cat").stop().animate({left:e.pageX, top:e.pageY});
+function randomize() {
+	console.log("randomize");
+	var which_link = Math.floor(Math.random() * 2);
+
+	var page1 = "./star.html";
+	var page2 = "./spaceWeight.html";
+
+	if (which_link === 0) {
+		window.location = page1;
+	}
+
+	else {
+        window.location = page2;
+	}
+}
+
+$(document).ready(function(){
+
+	$(document).mousemove(function(e){
+	    $("#space-cat").stop().animate({left:e.pageX, top:e.pageY});
+	});
+
+	$("img").on("click", function(){
+		console.log("click");
+		randomize();
+	});
+
 });
--- js/starTypes.js
+++ js/starTypes.js
@@ -7,27 +7,28 @@
 var starTypes = {
 	brownDwarf: {
 		name: "Brown Dwarf",
-		bio: "Congratulations! You made a brown dwarf. You didn't accumulate enough mass to create a star, so instead your brown dwarf will cool and fade over time. It weighs between about 7 and 8% of the mass of the Sun."
+		bio: "Congratulations! You made a brown dwarf. You didn't accumulate enough mass to create a star, so instead your brown dwarf will cool and fade over time. It weighs between about 7 and 8% of the Sun’s mass. Gliese 229b was one of the first brown dwarfs ever discovered!"
+
 	},
 
 	lowMassStar: {
 		name: "Low Mass Star",
-		bio: "You made a low mass star! It weighs between 8 and 80% of the Sun's mass and will live for about a trillion of years. Towards the end of its life, it will fizzle into a White Dwarf. White dwarfs are dense stellar embers that cool over billions of years."
+		bio: "You made a low mass star! It weighs between 8 and 80% of the Sun's mass and will live for about a trillion years. Towards the end of its life, it will fizzle into a White Dwarf. White dwarfs are dense stellar embers that cool over billions of years. One example of a low mass star is Barnard's Star!"
 	},
 
 	iMMassStar: {
 		name: "Intermediate Mass Star",
-		bio: "You've created a Sun-like star! These stars have masses ranging from 0.8-8 times that of the Sun and have a lifetime of abou 10 billion years. At the end of their lifetime, these stars have used up their primary fuel source and explode, becoming Red Giants (large low-temperature stars) which cool further to become stellar remnants called White Dwarfs."
+		bio: "You've created a Sun-like star! These stars have masses ranging from 0.8-8 times that of the Sun and have a lifetime of about 10 billion years. At the end of their lifetime, these stars have used up their primary fuel source and expand, becoming Red Giants (large low-temperature stars) which explode and cool further to become stellar remnants called White Dwarfs."
 	},
 
 	highMassStar: {
 		name: "High Mass Star",
-		bio: "Woah! That's a big star! I'd say it'll live for 10 million years. It is 8 to 20 times the mass of the Sun, and you know that the more massive a star, the shorter its lifespan will be. It's so big that once it has used up its primary energy source, it will expand into a Super Giant. The Super Giant will likely then explode in a Supernova and collapse into a Neutron star. Neutron stars are very small (not much bigger than Chicago!) and extremely dense."
+		bio: "Woah! That's a big star! I'd say it'll live for 10 million years. It is 8 to 20 times the mass of the Sun, and you know that the more massive a star, the shorter its lifespan will be. It's so big that once it’s used up its primary energy source, it will expand into a Super Giant. The Super Giant will likely then explode in a Supernova and collapse into a Neutron star. Neutron stars are very small (not much bigger than Chicago!) and extremely dense. Cassiopeia A is one example of a high mass star."
 	},
 
 	vHighMassStar: {
 		name: "Very High Mass Star",
-		bio: "You made a big one! The highest-mass stars range from 20-100 times the mass of our Sun, and when they have used up their primary fuel source, they expand and become supergiant stars, which are larger than any other star and vary widely in temperature. These supergiants explode in a supernov  and can either become a new star (play again!) or a black hole (light can't even escape this supermassive point!)."
+		bio: "You made a big one--maybe even as big as Cygnus X-1! The highest-mass stars range from 20-100 times the mass of our Sun, and when they have used up their primary fuel source, they expand and become supergiant stars, which are larger than any other star and vary widely in temperature. These supergiants explode in a supernova and can either become a new star (play again!) or a black hole (light can't even escape this supermassive point!)."
 	}
 
 };
@@ -49,7 +50,7 @@ function checkStarType(diameter) {
 		$(".name").html(starTypes.highMassStar.name);
 		$(".description").html(starTypes.highMassStar.bio);
 	}
-	else if (diameter > 350 && diameter <=600) {
+	else {
 		$(".name").html(starTypes.vHighMassStar.name);
 		$(".description").html(starTypes.vHighMassStar.bio);
 	}
